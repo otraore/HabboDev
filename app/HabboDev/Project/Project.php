569a01988e85c9525294d4e23e8f8e395ec2b18d
otraore <justdevinc@gmail.com>
2015-06-15 20:30:30 -0400

    Updated Multi-language system

---
+++ app/HabboDev/Project/Project.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace HabboDev\Project;
+
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class Project extends Eloquent
+{
+    protected $table = 'projects';
+
+    protected $fillable = [
+
+    ];
+
+}
